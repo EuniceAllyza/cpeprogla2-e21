9ca3bf41fb52dcb48bb4ba9ff53b8cca71c76143
Eunice Allyza A. Ruiz <euniceallyza_20@yahoo.com>
2016-10-22 09:42:43 +0800

    Add files via upload

---
+++ collatz.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+	ifstream f("input.txt");
+	int n, i, j;
+	while (f >> i >> j){
+	 	int tempi = i;
+        int tempj = j;
+        if(i>j)
+        swap(i,j);
+            
+        int maxCycle_Length = INT_MIN;
+        int cycle_Length;
+        while(i<=j)
+        {
+            n=i;
+            cycle_Length = 1;
+            while(n!=1){
+            	if(n%2!=0)
+                    n = (3*n)+1;
+                else
+                    n = n/2;
+                    cycle_Length++;
+            }
+                if(cycle_Length > maxCycle_Length)
+                    maxCycle_Length = cycle_Length;
+                    i++;
+        }
+        cout << tempi << " " << tempj << " " << maxCycle_Length << endl;
+	}
+	return 0;
+}
